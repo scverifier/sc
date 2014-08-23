cef0019a3592d341c5e8a3105343299bb1984c5d
Snapchat Verifier <applicationrootlogger@gmail.com>
2014-08-23 21:10:36 +0300

    Add subreddits list.

--- wsgi/openshift/verifier/urls.py
+++ wsgi/openshift/verifier/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from verifier.models import Gender, Subreddit
 from verifier.views import GenderCreateView, GenderListView
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^/genders/new$', GenderCreateView.as_view(), name='gender_create'),
     url(r'^/genders/(?P<pk>\d+)', GenderCreateView.as_view(), name='gender_edit'),
-    url(r'^/genders', GenderListView.as_view(), name='gender_edit'),
+    url(r'^/genders', login_required(ListView.as_view(
+        model=Gender, template_name='verifier/genders.html')), name='gender_list'),
+    url(r'^/subreddits', login_required(ListView.as_view(
+        model=Subreddit, template_name='verifier/subreddits.html')), name='subreddit_list'),
 )
