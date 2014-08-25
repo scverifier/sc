deddc9b7b626e9be45e8b3e533d1b54df7cd6873
scverifier <applicationrootlogger@gmail.com>
2014-08-25 18:54:21 +0300

    Removed redundant print.

--- wsgi/openshift/verifier/verification.py
+++ wsgi/openshift/verifier/verification.py
@@ -50,8 +50,3 @@ def verify(moderator_username, moderator_password,
         raise RedditAPIException(e)
     except praw.errors.ClientException as e:
         raise RedditAPIException(e)
-
-    print('Verified {user} by {moderator} with password {pwd} for {subreddit} with flair {flair_text} and {flair_css}.'.\
-        format(user=username, moderator=moderator_username, pwd=moderator_password, subreddit=subreddit,
-            flair_text=flair_text, flair_css=flair_css))
-    pass
